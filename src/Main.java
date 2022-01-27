8271fa016fcd2330bf854f75ebbf7752b75063a8
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2022-01-27 22:14:41 +0530

    FInal code

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,24 @@ import java.awt.*;
 
 public  class Main  {
     void getInterface(java.util.List<PcapIf> allDevices) {
-        // Frame
         JFrame frame = new JFrame();
-        frame.setTitle("Network Packet Sniffer");
+        frame.setTitle("NETWORK PACKET SNIFFER");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 550);
+        //frame.getContentPane().add(new JPanelWithBackground("packet.png "));
         frame.setLayout(new
 
                 BorderLayout());
-        frame.setBackground(Color.decode("#F5F5F5"));
+        // frame.setBackground(Color.decode("#F5F5F5"));
+        frame.setBackground(Color.cyan);
         frame.setVisible(true);
-
-        // Choose your interface
         JLabel label1 = new JLabel();
         label1.setText("Choose your interface");
         label1.setHorizontalAlignment(JLabel.LEFT);
         label1.setVerticalAlignment(JLabel.TOP);
         label1.setFont(new
 
-                Font("MV Bali", Font.BOLD, 25));
-
-        // @Copyrights Network Packet Sniffer
+                Font("MV Bali", Font.ITALIC, 25));
         JLabel label2 = new JLabel();
         label2.setText("@Copyrights Network Packet Sniffer");
         label2.setHorizontalAlignment(JLabel.LEFT);
@@ -33,80 +30,67 @@ public  class Main  {
         label2.setFont(new
 
                 Font("MV Bali", Font.PLAIN, 18));
-
-        // Capture the network traffic
         JLabel label3 = new JLabel("Capture the network traffic");
-
+        label3.setForeground(Color.red);
         label3.setHorizontalAlignment(JLabel.LEFT);
         label3.setVerticalAlignment(JLabel.TOP);
         label3.setFont(new
 
-                Font("MV Bali", Font.PLAIN, 20));
-
-        // Panel 1
+                Font("MV Bali", Font.BOLD, 20));
         JPanel panel1 = new JPanel();
         panel1.setPreferredSize(new
 
                 Dimension(80, 50));
-        panel1.setBackground(Color.decode("#F05454"));
-        panel1.add(label1);
-
-        // Panel2
-        JPanel panel2 = new JPanel();
-        panel2.setBackground(Color.decode("#F05454"));
-        panel2.setPreferredSize(new
-
-                Dimension(10, 50));
-        panel2.add(panel1);
-
-        // panel3
-        JPanel panel3 = new JPanel();
-        panel3.setPreferredSize(new
-
-                Dimension(300, 140));
-        panel3.setBackground(Color.decode("#F5F5F5"));
-        panel3.add(label3);
-
-        // panel4
         JPanel panel4 = new JPanel();
         panel4.setPreferredSize(new
 
                 Dimension(80, 40));
-        panel4.setBackground(Color.decode("#121212"));
+
+        panel1.add(label1);
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        panel3.setPreferredSize(new
+
+                Dimension(300, 140));
+       /* panel4.setBackground(Color.decode("#121212"));
+        panel3.setBackground(Color.decode("#F5F5F5"));
+        panel1.setBackground(Color.decode("#F05454"));
+        panel2.setBackground(Color.decode("#F05454"));*/
+        panel1.setBackground(Color.pink);
+        panel2.setBackground(Color.gray);
+//        panel3.setBackground(Color.CYAN);
+        panel4.setBackground(Color.gray);
+        panel2.setPreferredSize(new
+
+                Dimension(10, 50));
+        panel2.add(panel1);
         panel4.add(label2);
-
-
-
-        // List of interface displayed in the GUI
+        panel3.add(label3);
         int j =0;
         for (PcapIf device : allDevices) {
 
             String description = (device.getDescription() != null) ? device
-                    .getDescription() : device.getName();
-            // Button
+                    .getDescription() : "No description available";
             JButton button = new JButton(description);
             button.setBounds(50, 100, 95, 30);
-            button.setBorder(new RoundedBorder(30));
-            button.setBackground(Color.decode("#F05454"));
-            button.setForeground(Color.decode("#FFFFFF"));
-
-            // button border
             button.setBorder(new RoundedBorder(10));
+            button.setBackground(Color.black);
 
-
+            button.setForeground(Color.pink);
+            /*button.setBackground(Color.decode("#F05454"));
+            button.setForeground(Color.decode("#FFFFFF"));
+            */
+            button.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
             panel2.add(button);
             button.setFocusable(false);
             int finalJ = j;
-
-            //button functionality
             button.addActionListener(e -> {frame.dispose();new SpoofPage(allDevices.get(finalJ));
             });
             j++;
             panel2.setLayout(new GridLayout(j+1, 0, 0, 0));
         }
-
-        // Adding panels in the frame with their position in the frame
         frame.add(panel3,BorderLayout.LINE_START);
+//        frame.add(panel1, BorderLayout.NORTH);
         frame.add(panel2, BorderLayout.CENTER);
         frame.add(panel4,BorderLayout.SOUTH);
     }
--- src/SpoofPage.java
+++ src/SpoofPage.java
@@ -2,7 +2,8 @@ import org.jnetpcap.PcapIf;
 import javax.swing.*;
 import java.awt.*;
 
-public class SpoofPage  {
+public class SpoofPage
+{
     JFrame frame = new JFrame();
     JButton myButton1 = new JButton("Run");
     JButton myButton2 = new JButton("Stop");
@@ -20,10 +21,22 @@ public class SpoofPage  {
         // Getting the device from the Main page through the constructor
         this.device = finalJ;
         frame.setTitle("Network Packet Sniffer");
-        myButton1.setBounds(100,160,200,40);
+        myButton1.setBounds(400,400,800,70);
         myButton1.setFocusable(false);
-        myButton2.setBounds(100,160,200,40);
+        myButton1.setBackground(Color.black);
+        myButton1.setForeground(Color.white);
+        myButton1.setSize(40,40);
+        myButton1.setPreferredSize(new Dimension(70, 40));
+        myButton2.setPreferredSize(new Dimension(70, 40));
+
+        myButton2.setSize(40,40);
+        // myButton1.setFocusable(true);
+        myButton2.setBounds(400,400,800,70);
         myButton2.setFocusable(false);
+        myButton2.setBackground(Color.CYAN);
+        myButton2.setForeground(Color.orange);
+
+        //myButton2.setFocusable(true);
         myButton2.setVisible(false);
 
         Packet p = new Packet(device,errorBuffer,textArea);
@@ -47,10 +60,17 @@ public class SpoofPage  {
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
 
+
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.gray);
+        frame.add(panel2, BorderLayout.SOUTH);
+        panel2.setLayout(new FlowLayout());
+
         // Panel1
         JPanel panel1 = new JPanel();
         panel1.setBackground(Color.BLUE);
         panel1.setLayout(new FlowLayout());
+
         frame.getContentPane().add(scrollableTextArea,BorderLayout.CENTER);
         panel1.add(myButton1);
         panel1.add(myButton2);
